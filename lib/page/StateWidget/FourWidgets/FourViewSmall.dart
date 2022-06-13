666b004bbec4342d50407205584962a54c841d2e
zhanghg <huagang.zhang@dhc.com.cn>
2022-06-13 16:18:06 +0800

    flutter 学习0613

--- lib/page/StateWidget/FourWidgets/FourViewSmall.dart
+++ lib/page/StateWidget/FourWidgets/FourViewSmall.dart
@@ -1,21 +1,17 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:flutter02/page/StateWidget/FourStatelessWidget.dart';
 
-class FourViewSmall extends StatefulWidget{
+class FourViewSmall extends StatefulWidget {
   final Color profileIconColor;
+
   FourViewSmall(this.profileIconColor);
 
   @override
   State<StatefulWidget> createState() {
     return _FourViewState();
   }
-
 }
 
-class _FourViewState extends State<FourViewSmall>{
-
+class _FourViewState extends State<FourViewSmall> {
   List<String> textArray = ["one", "two"];
 
   @override
@@ -30,6 +26,4 @@ class _FourViewState extends State<FourViewSmall>{
       ),
     );
   }
-
-
 }
